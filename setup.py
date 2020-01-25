6fa70e87c365c86fafe68a292d0a36eb6470aa68
kokos <kokos1811@outlook.com.gr>
2020-01-25 10:39:00 +0200

    change version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("LICENSE", "r") as f:
 setup(
 	name="kokos",
 	author="kokos",
-	version="0.1.3",
+	version="0.1.4",
 	description="kokos is a multi-use package",
 	long_description=README,
 	long_description_content_type="text/markdown",
@@ -23,17 +23,17 @@ setup(
 	url="https://github.com/kokosxD/kokos",
 	download_url="https://github.com/kokosxD/kokos/archive/master.zip",
 	install_requires=[
-		"bcrypt>=3.1.7",
-		"datetime>=4.3",
+		"bcrypt==3.1.7",
+		"datetime==4.3",
 	],
 	project_urls={
 		"Documentation": "https://github.com/kokosxD/kokos/blob/master/README.md",
 		"Source Code": "https://github.com/kokosxD/kokos",
 	},
 	classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "License :: OSI Approved :: MIT License",
-    	"Operating System :: Microsoft :: Windows",
+    	"Operating System :: Microsoft :: Windows :: Windows 10",
     ],
 	scripts=[
 		"scripts/kokos.bat",
